fba094f2387d68d1ae8b45be203788aa4515d1ef
Glen Pierce <glen.piercev@gmail.com>
2019-01-25 21:49:47 -0800

    weird updates to player controller

--- GGJ2019/Assets/Scripts/PlayerController.cs
+++ GGJ2019/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    GameObject ship;
+    GameObject cube;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //ship = GetComponent<GameObject>();
+        cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.AddComponent<Rigidbody>();
+        cube.GetComponent<Rigidbody>().useGravity = false;
+        //cube.AddComponent<BoxCollider>();
+        cube.transform.position = new Vector3(0, 0, 3);
+        cube.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -60));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetComponent<Rigidbody>().AddForce(Vector3.forward);
     }
 }
