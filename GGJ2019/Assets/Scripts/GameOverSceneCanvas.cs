e5bf065e60fddac58948f2b8d32229dc4a085365
Yvonne Liu <yvonne@jostle.me>
2019-01-26 14:46:28 -0800

    Yvonne's work and Glen is responsible if this is terrible

---
+++ GGJ2019/Assets/Scripts/GameOverSceneCanvas.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverSceneCanvas : MonoBehaviour
+{
+    [SerializeField]
+    private Button exitButton;
+    [SerializeField]
+    private Button restartButton;
+    [SerializeField]
+    private Text endingMsg;
+    [SerializeField]
+    private Text scoreMsg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        exitButton.onClick.AddListener(ExitGame);
+        restartButton.onClick.AddListener(GoToStartScene);
+    }
+
+    void ExitGame()
+    {
+        Debug.Log("Exit button clicked");
+        endingMsg.text = "Thank you ~";
+        scoreMsg.enabled = false;
+        restartButton.interactable = false;
+        exitButton.interactable = false;
+    }
+
+    void GoToStartScene()
+    {
+        Debug.Log("Restart button clicked");
+        SceneManager.LoadScene("StartScene");
+    }
+}
---
+++ GGJ2019/Assets/Scripts/StartSceneCanvas.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartSceneCanvas: MonoBehaviour
+{
+    [SerializeField]
+    private Button startButton;
+    [SerializeField]
+    private Text buttonText;
+
+    void Start()
+    {
+        startButton.onClick.AddListener(HandleClick);
+    }
+
+    public void HandleClick()
+    {
+        Debug.Log("Start button clicked");
+        SceneManager.LoadScene("SampleScene");
+    }
+}
