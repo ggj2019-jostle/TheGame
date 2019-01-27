8d7a02fc753ca48018b33040d66ccd9a73e35498
Glen Pierce <glen.piercev@gmail.com>
2019-01-27 12:35:28 -0800

    adding new planets

--- GGJ2019/Assets/Scripts/PlayerController.cs
+++ GGJ2019/Assets/Scripts/PlayerController.cs
@@ -16,6 +16,11 @@ public class PlayerController : MonoBehaviour
         GetComponent<Rigidbody>().drag = drag;
     }
 
+    void OnCollisionEnter(Collision collision) {
+        Debug.Log("collision");
+        GetComponent<Rigidbody>().velocity = -GetComponent<Rigidbody>().velocity;
+    }
+
     void Update()
     {
         bool turning = false;
@@ -56,8 +61,8 @@ public class PlayerController : MonoBehaviour
             GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
         }
 
-        Vector3 currentPosition = GetComponent<Rigidbody>().transform.position;
+        // Vector3 currentPosition = GetComponent<Rigidbody>().transform.position;
 
-        GetComponent<Rigidbody>().transform.position = new Vector3(currentPosition.x, 0, currentPosition.z);
+        // GetComponent<Rigidbody>().transform.position = new Vector3(currentPosition.x, 0, currentPosition.z);
     }
 }
