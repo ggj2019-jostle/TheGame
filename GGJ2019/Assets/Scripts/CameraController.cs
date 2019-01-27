d8d9c6aee6ee3d287932340187d93c405aaf099f
Glen Pierce <glen.piercev@gmail.com>
2019-01-26 20:56:36 -0800

    i still can't get the banking right so it's turned off for now

---
+++ GGJ2019/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject player = GameObject.FindGameObjectsWithTag("Player")[0];
+        transform.position = new Vector3(player.transform.position.x - 20, transform.position.y, player.transform.position.z + 140);
+    }
+}
