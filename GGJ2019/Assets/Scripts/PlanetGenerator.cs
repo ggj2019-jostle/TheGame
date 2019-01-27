f8f04f78ff3a54cb67fb9dfe29824318774bf9c8
Yvonne Liu <yvonne@jostle.me>
2019-01-27 12:37:01 -0800

    add star types

--- GGJ2019/Assets/Scripts/PlanetGenerator.cs
+++ GGJ2019/Assets/Scripts/PlanetGenerator.cs
@@ -10,38 +10,104 @@ public class PlanetGenerator : MonoBehaviour
     public int minRFromSun = 20;
     public int maxRFromSun = 150;
 
-    public int minSpeed = 10;
-    public int maxSpeed = 100;
+    public int minTranslationSpeed = 10;
+    public int maxTranslationSpeed = 100;
+    public int minRotationSpeed = 20;
+    public int maxRotationSpeed = 100;
 
-    public void SetUp(int amountOfPlanets, CelestialBody star)
+    public int numOfPlanetTypes = 9;
+
+
+    public void SetUp(int amountOfPlanets, CelestialBody star, bool ensureHabitat)
     {
-
+        int r;
+        int translationSpeed;
+        int rotationSpeed;
+        int planetType;
+        bool haveHabitat = true;
         for (int i = 0; i < amountOfPlanets; i++)
         {
             Debug.Log("Populate planet: " + i);
-            int r = Random.Range(minRFromSun, maxRFromSun);
-            int s = Random.Range(minSpeed, maxSpeed);
-            addPlanet(r, s, star);
+            r = Random.Range(minRFromSun, maxRFromSun);
+            translationSpeed = Random.Range(minTranslationSpeed, maxTranslationSpeed);
+            rotationSpeed = Random.Range(minRotationSpeed, maxRotationSpeed);
+            if (ensureHabitat && !haveHabitat && i == amountOfPlanets - 1)
+            {
+                planetType = 1;
+            } else
+            {
+                planetType = Random.Range(1, numOfPlanetTypes + 1);
+            }
+            if (ensureHabitat)
+            {
+                if(planetType == 1)
+                {
+                    haveHabitat = true;
+                }
+                addPlanet(r, translationSpeed, rotationSpeed, star, planetType);
+            }
+            else
+            {
+                addPlanet(r, translationSpeed, rotationSpeed, star, planetType);
+            }
         }
     }
 
-    private void addPlanet(int r, int speed, CelestialBody sun)
+    private void addPlanet(int r, int translationSpeed, int rotationSpeed, CelestialBody star, int planetType)
     {
-        Debug.Log("Populate planet: R = " + r + " , sun: " + sun);
-        Vector3 sunPosition = sun.transform.position;
-  
-        CelestialBody planet = Instantiate(Resources.Load<CelestialBody>("nicePlanet"));
-        planet._star = sun;
-        planet.TranslationSpeed = speed;
+        Debug.Log("Populate planet: R = " + r + " , sun: " + star);
+        Vector3 starposition = star.transform.position;
+
+
+        CelestialBody planet;
+
+        switch (planetType)
+        {
+            case 1:
+                planet = Instantiate(Resources.Load<CelestialBody>("nicePlanet"));
+                break;
+            case 2:
+                planet = Instantiate(Resources.Load<CelestialBody>("nicePlanet"));
+                break;
+            case 3:
+                planet = Instantiate(Resources.Load<CelestialBody>("nicePlanet"));
+                break;
+            case 4:
+                planet = Instantiate(Resources.Load<CelestialBody>("nicePlanet"));
+                break;
+            case 5:
+                planet = Instantiate(Resources.Load<CelestialBody>("nicePlanet"));
+                break;
+            case 6:
+                planet = Instantiate(Resources.Load<CelestialBody>("nicePlanet"));
+                break;
+            case 7:
+                planet = Instantiate(Resources.Load<CelestialBody>("nicePlanet"));
+                break;
+            case 8:
+                planet = Instantiate(Resources.Load<CelestialBody>("nicePlanet"));
+                break;
+            case 9:
+                planet = Instantiate(Resources.Load<CelestialBody>("nicePlanet"));
+                break;
+            default:
+                planet = Instantiate(Resources.Load<CelestialBody>("nicePlanet"));
+                break;
+        }
+
+
+        planet._star = star;
+        planet.TranslationSpeed = translationSpeed;
+        planet.RotationSpeed = rotationSpeed;
 
         int randPosition = Random.Range(0, 2);
         if (randPosition == 0)
         {
-            planet.transform.position = new Vector3(sunPosition.x + r, 0, sunPosition.z);
+            planet.transform.position = new Vector3(starposition.x + r, 0, starposition.z);
         }
         else
         {
-            planet.transform.position = new Vector3(sunPosition.x, 0, sunPosition.z + r);
+            planet.transform.position = new Vector3(starposition.x, 0, starposition.z + r);
         }
         
     }
--- GGJ2019/Assets/Scripts/StarGenerator.cs
+++ GGJ2019/Assets/Scripts/StarGenerator.cs
@@ -7,6 +7,8 @@ public class StarGenerator : MonoBehaviour
     public int minNumberOfStars = 3;
     public int maxNumberOfStars = 5;
 
+    public int typesOfStar = 2;
+
     private PlanetGenerator planetGenerator = new PlanetGenerator();
 
     public void SetUp(int numberOfStars)
@@ -15,23 +17,39 @@ public class StarGenerator : MonoBehaviour
 
         int x;
         int z;
+        int starType;
 
         for (int i = 0; i < numberOfStars; i++)
         {
             Debug.Log("Populate star: " + i);
             x = Random.Range(SampleSceneController.minX, SampleSceneController.maxX);
             z = Random.Range(SampleSceneController.minZ, SampleSceneController.maxZ);
-            addStar(x, z);
+            starType = Random.Range(0, typesOfStar);
+            if (i == numberOfStars - 1)
+            {
+                addStar(x, z, starType, true);
+            } else
+            {
+                addStar(x, z, starType, false);
+            }
+            
         }
     }
 
-    private void addStar(int x, int z)
+    private void addStar(int x, int z, int starType, bool lastone)
     {
-        var debug = Resources.Load<CelestialBody>("PS_BlueSun");
-        CelestialBody instance = Instantiate(debug);
-        instance.transform.position = new Vector3(x, 0, z);
+        CelestialBody star;
+        if (starType == 0)
+        {
+            star = Instantiate(Resources.Load<CelestialBody>("PS_BlueSun"));
+        }
+        else
+        {
+            star = Instantiate(Resources.Load<CelestialBody>("PS_Sun"));
+        }
+        star.transform.position = new Vector3(x, 0, z);
 
         int numberOfPlanets = Random.Range(planetGenerator.minNumberOfPlanets, planetGenerator.maxNumberOfPlanets);
-        planetGenerator.SetUp(numberOfPlanets, instance);
+        planetGenerator.SetUp(numberOfPlanets, star, lastone);
     }
 }
